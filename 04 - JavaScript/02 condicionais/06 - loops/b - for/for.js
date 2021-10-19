7d64c47a82ede608283f0144a7d8b286be12c50a
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-18 23:22:52 -0300

    Exercício: for

--- 04 - JavaScript/02 condicionais/06 - loops/b - for/for.js
+++ 04 - JavaScript/02 condicionais/06 - loops/b - for/for.js
@@ -1,15 +1,32 @@
-//Aula - substituir todos os caracteres "e" pelo caractere "i"
-let spaceship = "Helmet";
-let newSpaceship = "";
+/*
+Aula - substituir todos os caracteres "e" pelo caractere "i"
 
-for (let i = 0; i < spaceship.length; i++) {
-  if (spaceship[i] == "e") {
-    newSpaceship += "i";
+    let spaceship = "Helmet";
+    let newSpaceship = "";
+
+    for (let i = 0; i < spaceship.length; i++) {
+      if (spaceship[i] == "e") {
+        newSpaceship += "i";
+      } else {
+        newSpaceship += spaceship[i]; //recebe o caractere normal
+      }
+    }
+
+    console.log(newSpaceship);
+    
+*/
+
+// Exercício:
+let spaceshipName = prompt("Nome da nave: ");
+let spaceshipChosenChar = prompt("Qual caractere você deseja substituir?");
+let spaceshipCharReplace = prompt("Por qual caractere deseja substituir?");
+let spaceshipNewName = "";
+
+for (let i = 0; i < spaceshipName.length; i++) {
+  if (spaceshipName[i] == spaceshipChosenChar) {
+    spaceshipNewName += spaceshipCharReplace;
   } else {
-    newSpaceship += spaceship[i]; //recebe o caractere normal
+    spaceshipNewName += spaceshipName[i];
   }
 }
-
-console.log(newSpaceship);
-
-//Exercício
+console.log("O nome novo da nave é: " + spaceshipNewName);
