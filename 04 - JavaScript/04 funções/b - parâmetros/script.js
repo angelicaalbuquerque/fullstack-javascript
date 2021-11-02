3372068444372fa32b298d10c0f1bc280a4c2040
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-11-02 09:54:04 -0300

    Funções - parâmetros

---
+++ 04 - JavaScript/04 funções/b - parâmetros/script.js
@@ -0,0 +1,10 @@
+// Parâmetros
+
+function speedUp(velocity, acceleration) {
+  let newVelocity = velocity + acceleration;
+  console.log("Nova velocidade: " + newVelocity);
+}
+
+speedUp(60, 10);
+speedUp(40, 20);
+speedUp(20, 30);
