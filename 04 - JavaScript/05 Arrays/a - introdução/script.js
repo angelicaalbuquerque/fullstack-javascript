998708136395f61a5763c96d8535de32b1224101
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-11-03 16:05:18 -0300

    Arrays - introdução

---
+++ 04 - JavaScript/05 Arrays/a - introdução/script.js
@@ -0,0 +1,11 @@
+// let hitchedSpaceships = ["Supernova", "Elemental", "Helmet"];
+
+// console.log(hitchedSpaceships[0]);
+
+// let spaceshipInfo = ["Colossus", 7, true];
+
+// console.log(spaceshipInfo[2]);
+
+//Cria um array com a QUANTIDADE de posições, mas totalmente vazio
+let getNumber = new Array(5);
+console.log(getNumber);
