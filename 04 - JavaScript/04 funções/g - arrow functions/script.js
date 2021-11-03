ce21035d242bf2c1b3556d3e58df180f829cdb8f
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-11-02 21:44:56 -0300

    Funções - arrow functions

---
+++ 04 - JavaScript/04 funções/g - arrow functions/script.js
@@ -0,0 +1,11 @@
+const doubleVelocity = (velocity) => {
+  return velocity * 2;
+};
+console.log(doubleVelocity(60));
+
+//arrow function simplificada com apenas uma linha de retorno
+const doubleVelocity = (velocity) => velocity * 2;
+console.log(doubleVelocity(60));
+
+const doubleVelocity = (velocity) => velocity * 2;
+console.log(doubleVelocity(60));
