05fbd10f87c02d8a960c3877745bdd1626693377
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-11-03 15:29:58 -0300

    High order functions

---
+++ 04 - JavaScript/04 funções/h - high order functions/script.js
@@ -0,0 +1,17 @@
+function doubleVelocity(velocity, printer) {
+  console.log(`Entrei em doubleVelocity`);
+  let newVelocity = velocity * 2;
+  printer(newVelocity);
+  return newVelocity;
+}
+
+// let printVelocity = (velocity) => {
+//   console.log("Nova velocidade: " + velocity + "km/s");
+// };
+
+// let newVelocity = doubleVelocity(60, printVelocity);
+// console.log(newVelocity);
+
+let anotherVelocity = doubleVelocity(50, (velocity) => {
+  console.log(`outra velocidade ${velocity}`);
+});
