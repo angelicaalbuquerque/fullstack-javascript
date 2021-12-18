be3de005ece5d2c8bd8a2186378d25e7abde53e9
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-12-18 19:44:02 -0300

    Enviando o DOM para a função

---
+++ 04 - JavaScript/09 Manipulando o DOM/f Enviando o DOM para a função/js/index.js
@@ -0,0 +1,8 @@
+function sayMyFirstName(e) {
+  alert("Meu primeiro nome é: " + e.value);
+}
+
+function sayMyLastName() {
+  console.log(event);
+  alert("Meu ultimo nome é: " + event.target.value);
+}
