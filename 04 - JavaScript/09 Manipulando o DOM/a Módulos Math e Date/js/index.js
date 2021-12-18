9739cb9d1abebc3081e5adcd4f6bbb857977e3f3
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-12-18 18:55:15 -0300

    Módulos Math e Date

---
+++ 04 - JavaScript/09 Manipulando o DOM/a Módulos Math e Date/js/index.js
@@ -0,0 +1,11 @@
+console.log(Math.pow(5, 3));
+console.log(Math.PI);
+console.log(Math.sqrt(64));
+
+let myBirthday = new Date(1990, 0, 09);
+console.log(myBirthday);
+//no DATE, o primeiro mÊs é 0
+
+let today = Date.now();
+console.log(today);
+//retorna a quantidade de milliseconds decorridos da data base do JavaScript, desde 01/01/1970
