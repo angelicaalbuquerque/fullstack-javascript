eaacd6401e2480502e2e5142b4adc5c127dbda43
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-12-21 15:58:11 -0300

    Desestruturação de array com função

--- 05 - JavaScript ES6+/01 Evolução e compatibilidade/c Desestruturação de Objetos e Arrays/js/index.js
+++ 05 - JavaScript ES6+/01 Evolução e compatibilidade/c Desestruturação de Objetos e Arrays/js/index.js
@@ -4,4 +4,9 @@ let [colossus, artemis] = spaceships;
 // let colossus = spaceships[0];
 // let artemis = spaceships[1];
 
-console.log(colossus, artemis);
+function printSpaceships([colossus, artemis, fenix, puller]) {
+  console.log(colossus, artemis, fenix, puller);
+}
+
+printSpaceships(spaceships);
+//cada elemento do array spaceships vai ser mapeado para uma variável definida em printSpaceships. Como não existe o elemento respectivo a puller, imprime undefined.
