79266e516b72c1a5fc946f5713d86765fffebb3e
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-12-21 16:12:48 -0300

    Spread

--- 05 - JavaScript ES6+/01 Evolução e compatibilidade/d Operador Spread/js/index.js
+++ 05 - JavaScript ES6+/01 Evolução e compatibilidade/d Operador Spread/js/index.js
@@ -1,9 +1,8 @@
-let spaceships = ["Colossus", "Ártemis", "Fenix"];
-console.log(spaceships); //imprime como array
-console.log(...spaceships); //imprime três strings distintas, desempacotando o array.
+function speedUp(velocity, acceleration) {
+  return velocity + acceleration;
+}
 
-let newSpaceships = [...spaceships, "Puller"];
-console.log(newSpaceships); //imprime array com os 3 de spaceships +  "Puller", considerando elemento a elemento:  ['Colossus', 'Ártemis', 'Fenix', 'Puller']
+let spaceshipAcceleration = [60, 10];
 
-let newSpaceships2 = [spaceships, "Puller"];
-console.log(newSpaceships2); //considera o array + um elemento: [Array(3), 'Puller']
+let newVelocity = speedUp(...spaceshipAcceleration);
+console.log(newVelocity); //70
