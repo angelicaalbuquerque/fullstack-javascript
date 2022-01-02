5f144413c5b09b6955a34700168bf810c2e48eb2
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-01-02 20:50:49 -0300

    Limpeza do app.js

--- 06 - React/my-first-app/src/App.js
+++ 06 - React/my-first-app/src/App.js
@@ -1,15 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello! 😃 </p>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
