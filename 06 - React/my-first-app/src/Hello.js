f7de3cfd01b9b43e6f34c9b35258a3b001f7cb62
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-01-02 20:57:44 -0300

    Arrow function e classe no component Hello

--- 06 - React/my-first-app/src/Hello.js
+++ 06 - React/my-first-app/src/Hello.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-function HelloWorld() {
-  return <h1>Hello World</h1>;
+class HelloWorld extends React.Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
 }
+
+// const HelloWorld = () => {
+//   return <h1>Hello World</h1>;
+// };
 
 export default HelloWorld;
