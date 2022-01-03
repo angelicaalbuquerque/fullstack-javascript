e33d73fbec4e5f62fa471e2577175ae60584e7c5
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-01-02 21:04:25 -0300

    Criando um component Lorem Ipsum

--- 06 - React/my-first-app/src/App.js
+++ 06 - React/my-first-app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import LoremIpsum from "./components/Lorem";
 import HelloWorld from "./Hello";
 
 function App() {
   return (
     <div>
       <HelloWorld />
+      <LoremIpsum />
     </div>
   );
 }
---
+++ 06 - React/my-first-app/src/components/Lorem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LoremIpsum = () => {
+  return (
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum assumenda
+      voluptas ex exercitationem ipsam odit necessitatibus, quaerat maxime
+      consequatur deleniti, dicta eius illo sequi, numquam suscipit aliquid.
+      Aperiam!
+    </p>
+  );
+};
+
+export default LoremIpsum;
