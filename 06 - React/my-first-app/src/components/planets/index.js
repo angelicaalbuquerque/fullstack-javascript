2a134c788a02a27ccfae9d94fd983fe67da29d0a
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-01-04 00:07:02 -0300

    Organizando conteúdo com props

--- 06 - React/my-first-app/src/components/planets/index.js
+++ 06 - React/my-first-app/src/components/planets/index.js
@@ -6,15 +6,19 @@ const Planets = () => {
     <Fragment>
       <h3>Lista de Planetas</h3>
       <hr />
-      <Planet />
-      <Planet />
-      <Planet />
-      <Planet />
-      <Planet />
-      <Planet />
-      <Planet />
-      <Planet />
-      <Planet />
+      <Planet
+        name="Mercurio"
+        descripton="Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o
+        Sol a cada 87,969 dias terrestres. A sua órbita tem a maior
+        excentricidade e o seu eixo apresenta a menor inclinação em relação ao
+        plano da órbita dentre todos os planetas do Sistema Solar."
+        img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"
+      />
+      <Planet
+        name="Plutão"
+        descripton="Plutão, formalmente designado 134340 Plutão, é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol."
+        img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
+      />
     </Fragment>
   );
 };
--- 06 - React/my-first-app/src/components/planets/planet/index.js
+++ 06 - React/my-first-app/src/components/planets/planet/index.js
@@ -1,18 +1,12 @@
 import React from "react";
 import GrayImg from "../../shared/gray_img";
 
-const Planet = () => {
+const Planet = (props) => {
   return (
     <div>
-      <h4>Mercúrio</h4>
-      <p>
-        Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o
-        Sol a cada 87,969 dias terrestres. A sua órbita tem a maior
-        excentricidade e o seu eixo apresenta a menor inclinação em relação ao
-        plano da órbita dentre todos os planetas do Sistema Solar.
-      </p>
-      <GrayImg img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg" />
-      <GrayImg img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Internal_Structure_of_Mercury_%28pt%29.jpg/220px-Internal_Structure_of_Mercury_%28pt%29.jpg" />
+      <h4>{props.name}</h4>
+      <p>{props.descripton}</p>
+      <GrayImg img_url={props.img_url} />
     </div>
   );
 };
