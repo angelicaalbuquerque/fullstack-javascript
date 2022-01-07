d23f2dbc46e5bf0160b34df98b6aeb402113f16c
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-01-07 19:15:55 -0300

    Exercicio: transformação de component em classe

--- 06 - React/my-first-app/src/components/planets/planet/index.js
+++ 06 - React/my-first-app/src/components/planets/planet/index.js
@@ -2,31 +2,33 @@ import React from "react";
 import DescriptonWithLink from "../../shared/DescriptionWithLink";
 import GrayImg from "../../shared/gray_img";
 
-const Planet = (props) => {
-  let title;
+class Planet extends React.Component {
+  render() {
+    let title;
 
-  if (props.title_with_underline) {
-    title = (
-      <h4>
-        <u>{props.name}</u>
-      </h4>
+    if (this.props.title_with_underline) {
+      title = (
+        <h4>
+          <u>{this.props.name}</u>
+        </h4>
+      );
+    } else {
+      title = <h4>{this.props.name}</h4>;
+    }
+
+    return (
+      <div onClick={() => this.props.clickOnPlanet(this.props.name)}>
+        {title}
+        <DescriptonWithLink
+          description={this.props.description}
+          link={this.props.link}
+          link_description={this.props.link_description}
+        />
+        <GrayImg img_url={this.props.img_url} gray={this.props.gray} />
+        <hr />
+      </div>
     );
-  } else {
-    title = <h4>{props.name}</h4>;
   }
-
-  return (
-    <div onClick={() => props.clickOnPlanet(props.name)}>
-      {title}
-      <DescriptonWithLink
-        description={props.description}
-        link={props.link}
-        link_description={props.link_description}
-      />
-      <GrayImg img_url={props.img_url} gray={props.gray} />
-      <hr />
-    </div>
-  );
-};
+}
 
 export default Planet;
