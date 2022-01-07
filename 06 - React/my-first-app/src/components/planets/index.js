f966089d699e399cb67b6254e6a13cd0f214b6e9
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-01-07 13:50:38 -0300

    Exibindo planetas com state

--- 06 - React/my-first-app/src/components/planets/index.js
+++ 06 - React/my-first-app/src/components/planets/index.js
@@ -1,36 +1,53 @@
 import React, { Fragment } from "react";
 import Planet from "./planet";
 
-const clickOnPlanet = (name) => {
-  console.log(`Um click no planeta ${name}`);
-};
-
 class Planets extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      planets: [
+        {
+          name: "Mercúrio",
+          description:
+            "Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar.",
+          link: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)",
+          link_description: "Saiba mais",
+          img_url:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg",
+        },
+        {
+          name: "Plutão",
+          description:
+            "Plutão, formalmente designado 134340 Plutão, é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol. ",
+          link: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)",
+          link_description: "Saiba mais",
+          img_url:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg",
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <Fragment>
         <h3>Lista de Planetas</h3>
         <hr />
+
         <Planet
-          name="Mercúrio"
-          description="Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o
-        Sol a cada 87,969 dias terrestres. A sua órbita tem a maior
-        excentricidade e o seu eixo apresenta a menor inclinação em relação ao
-        plano da órbita dentre todos os planetas do Sistema Solar."
-          link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
-          link_description="Saiba mais"
-          img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"
-          clickOnPlanet={clickOnPlanet}
-          title_with_underline={true}
-          gray={true}
+          name={this.state.planets[0].name}
+          description={this.state.planets[0].description}
+          link={this.state.planets[0].link}
+          link_description={this.state.planets[0].link_description}
+          img_url={this.state.planets[0].img_url}
         />
         <Planet
-          name="Plutão"
-          description="Plutão, formalmente designado 134340 Plutão, é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol. "
-          // link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
-          link_description="Saiba mais"
-          img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
-          clickOnPlanet={clickOnPlanet}
+          name={this.state.planets[1].name}
+          description={this.state.planets[1].description}
+          link={this.state.planets[1].link}
+          link_description={this.state.planets[1].link_description}
+          img_url={this.state.planets[1].img_url}
         />
       </Fragment>
     );
