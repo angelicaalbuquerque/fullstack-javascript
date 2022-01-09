9697f4dfdbb7336ad5ea07fe5c032cf1252c50c6
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-01-09 00:01:56 -0300

    Exercício: Refatorando component Planet com Hooks

--- 06 - React/my-first-app/src/components/planets/planet/index.js
+++ 06 - React/my-first-app/src/components/planets/planet/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DescriptonWithLink from "../../shared/DescriptionWithLink";
 import GrayImg from "../../shared/gray_img";
 
@@ -9,54 +9,45 @@ async function getSatellites(id) {
   return data;
 }
 
-class Planet extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      satellites: [],
-    };
-  }
+const Planet = (props) => {
+  const [satellites, setSatellites] = useState([]);
 
-  componentDidMount() {
-    getSatellites(this.props.id).then((data) => {
-      this.setState((state) => ({
-        satellites: data["satellites"],
-      }));
+  useEffect(() => {
+    getSatellites(props.id).then((data) => {
+      setSatellites(data["satellites"]);
     });
-  }
+  }, []);
 
-  render() {
-    let title;
+  let title;
 
-    if (this.props.title_with_underline) {
-      title = (
-        <h4>
-          <u>{this.props.name}</u>
-        </h4>
-      );
-    } else {
-      title = <h4>{this.props.name}</h4>;
-    }
-
-    return (
-      <div onClick={() => this.props.clickOnPlanet(this.props.name)}>
-        {title}
-        <DescriptonWithLink
-          description={this.props.description}
-          link={this.props.link}
-          link_description={this.props.link_description}
-        />
-        <GrayImg img_url={this.props.img_url} gray={this.props.gray} />
-        <ul>
-          {this.state.satellites.map((satellite, index) => (
-            <li key={index}>{satellite.name}</li>
-          ))}
-        </ul>
-
-        <hr />
-      </div>
+  if (props.title_with_underline) {
+    title = (
+      <h4>
+        <u>{props.name}</u>
+      </h4>
     );
+  } else {
+    title = <h4>{props.name}</h4>;
   }
-}
+
+  return (
+    <div onClick={() => props.clickOnPlanet(props.name)}>
+      {title}
+      <DescriptonWithLink
+        description={props.description}
+        link={props.link}
+        link_description={props.link_description}
+      />
+      <GrayImg img_url={props.img_url} gray={props.gray} />
+      <ul>
+        {satellites.map((satellite, index) => (
+          <li key={index}>{satellite.name}</li>
+        ))}
+      </ul>
+
+      <hr />
+    </div>
+  );
+};
 
 export default Planet;
